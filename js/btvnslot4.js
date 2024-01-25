a7e4e5e42d81bf13710c82a76315196c4fc21b7b
Đinh Hoài Nam <namhoanq0108@gmail.com>
2024-01-26 03:46:13 +0700

    Push up BTVN slot 4

---
+++ js/btvnslot4.js
@@ -0,0 +1,50 @@
+/*Chương trình Vietlot:
+Người chơi sẽ mua 1 vé với 1 bộ 6 số khác nhau (từ 0-> 45) với giá 10.000VND
+
+Khi đến kỳ quay, máy tính sẽ tạo ra 1 bộ 6 số ngẫu nhiên khác nhau (từ 0->45).
+
+Hãy kiểm tra 2 bộ số này xem có bao nhiêu số trong đó giống nhau.
+
+Nếu 
+
+1 hoặc 2 số thì không trúng giải
+
+3 số thì trúng 100.000VND
+
+4 số trúng 10.000.000VND
+
+5 số trúng giải Jackpot (100 tỷ) */
+
+for(var i=1; i<=6; i++)
+{
+    for(var j=1; j<=6; j++)
+        {
+            var kq = [];
+
+            var kqRandom = Math.random() * 45;
+            kqRandom = parseInt(kqRandom);
+            kq.push(kqRandom)
+        }
+
+    var arrSelector = [];
+
+    var randomNumber = Math.random() * 45;
+    randomNumber = parseInt(randomNumber);
+    arrSelector.push(randomNumber);
+
+    var numberSelector = prompt(`Nhập số muốn đặt thứ ${i} : `);
+    numberSelector = parseInt(numberSelector);
+
+    arrSelector.forEach(e=>
+        {
+            if(e == numberSelector)
+            {
+                alert("Bạn đã trùng");
+            }
+            else
+            {
+                alert("Chúc bạn may mắn lần sau");
+            }
+        })
+}
+
